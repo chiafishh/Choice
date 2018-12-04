b361134b4b29ba29cc6a8567a93fe267777f6e02
andy <andy870622@gmail.com>
2018-12-04 21:04:06 +0800

    Initial Commit

---
+++ Choice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Choice
+//
+//  Created by User15 on 2018/12/4.
+//  Copyright © 2018 Chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
